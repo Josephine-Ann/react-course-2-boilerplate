293cbd465645518d6c5d8205516535db91fb5db3
Josephine-Ann <josephineanngeoghegan@gmail.com>
2020-06-15 10:50:35 +0100

    s

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,8 @@ export const Header = ({ startLogout }) => (
     <header className="header">
         <div className="content-container">
             <div className="header__content">
-                <Link className="header__title" to="/dashboard">
-                <h1>Expensify</h1>
+                <Link className="header__title" to="/flashcarddashboard">
+                <h1>Flashcard App</h1>
                 </Link>
                 <button className="button button--link" onClick={startLogout}>Log out</button>
                 </div>
