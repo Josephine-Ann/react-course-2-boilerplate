e861bbd9f74d402a56e36b0ef57df58cc05f6fa7
Josephine-Ann <josephineanngeoghegan@gmail.com>
2020-06-21 14:56:22 +0100

    s

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 var config = {
-  apiKey: "process.env.FIREBASE_API_KEY",
-  authDomain: "process.env.FIREBASE_AUTH_DOMAIN",
-  databaseURL: "process.env.FIREBASE_DATABASE_URL",
-  projectId: "process.env.FIREBASE_PROJECT_ID",
-  storageBucket: "process.env.FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "process.env.FIREBASE_MESSAGING_SENDER_ID"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   // apiKey: "AIzaSyDjvSnQBMp-ohtSIqNIK6iizsb602x3WwY",
   // authDomain: "flashcard-app-182a6.firebaseapp.com",
   // databaseURL: "https://flashcard-app-182a6.firebaseio.com",
