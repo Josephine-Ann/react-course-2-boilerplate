afc77d70b3163f4d2256c3e8fe7dc1adf8840e07
Josephine-Ann <josephineanngeoghegan@gmail.com>
2020-06-21 19:23:43 +0100

    s

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,14 +7,6 @@ var config = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-  // apiKey: "AIzaSyDjvSnQBMp-ohtSIqNIK6iizsb602x3WwY",
-  // authDomain: "flashcard-app-182a6.firebaseapp.com",
-  // databaseURL: "https://flashcard-app-182a6.firebaseio.com",
-  // projectId: "flashcard-app-182a6",
-  // storageBucket: "flashcard-app-182a6.appspot.com",
-  // messagingSenderId: "673672469213",
-  // appId: "1:673672469213:web:20d6140ecf865dd0ce5934",
-  // measurementId: "G-MDETWQP62G"
   };
 
   console.log(`Database url is :${process.env.FIREBASE_DATABASE_URL}`);
