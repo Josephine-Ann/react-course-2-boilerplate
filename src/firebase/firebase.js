ec1a5a431799309e4ca3d46d1b8f01c8f896c2dd
Josephine-Ann <josephineanngeoghegan@gmail.com>
2020-06-21 09:29:55 +0100

    s

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,7 +2,7 @@ import * as firebase from 'firebase';
 
 var config = {
   apiKey:"process.env.FIREBASE_API_KEY",
-  authDomain: "process.env.FIREBASE_AUTH_DOMAIN",
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL:"process.env.FIREBASE_DATABASE_URL",
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
