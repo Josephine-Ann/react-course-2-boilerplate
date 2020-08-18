75c921ac35168fa11b476cb5f5efe1d6ef2587ae
Josephine-Ann <josephineanngeoghegan@gmail.com>
2020-08-18 19:06:44 +0200

    ´sBÑH

--- src/components/NotificationModal.js
+++ src/components/NotificationModal.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Modal from 'react-modal';
 
 const NotificationModal = (props) => (
-    <Modal 
-    isOpen={!!props.modalOpen}
-    contentLabel="modalOpen"
-    onRequestClose={props.clearStateCloseModal}
-    className="modal"
+    <Modal
+        isOpen={!!props.modalOpen}
+        contentLabel="modalOpen"
+        onRequestClose={props.clearStateCloseModal}
+        className="modal"
     >
-       <p className="modal__body">Now continue saying the word in English, although you now see it in Spanish!</p>
-       <button className="button" onClick={props.clearStateCloseModal}>Okay!</button>
+        <p className="modal__body">Now continue to say the word in English, although you now see it in Spanish!</p>
+        <button className="button" onClick={props.clearStateCloseModal}>Okay!</button>
     </Modal>
 );
 
